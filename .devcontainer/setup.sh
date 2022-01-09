fab3f408727456827f52727a4e281f9957425d4f
Naomi Rose <23469+nomiro@users.noreply.github.com>
2022-01-09 15:05:53 +0000

    Add more packages and configure bash completion

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,32 +7,78 @@ cd "${tmpdir}"
 # Alpine
 # -----------------------------------------------------------------------------
 
+cat >/etc/apk/repositories <<EOF
+https://dl-cdn.alpinelinux.org/alpine/edge/main
+https://dl-cdn.alpinelinux.org/alpine/edge/testing
+https://dl-cdn.alpinelinux.org/alpine/edge/community
+EOF
+
 apk update
 apk upgrade
 
 apk add --no-cache \
+    bash-completion \
+    util-linux \
+    util-linux-doc \
+    util-linux-bash-completion \
+    git \
+    git-doc \
+    git-bash-completion \
+    github-cli \
+    github-cli-doc \
+    github-cli-bash-completion \
+    editorconfig-checker \
+    yamllint \
+    prettier \
+    shfmt \
+    shfmt-doc \
     python3 \
+    python3-doc \
     python3-dev \
     py3-pip \
+    py3-pip-doc \
+    py3-pip-bash-completion \
     py3-wheel \
-    shfmt \
-    bash-completion
-
-ensure_bash_completion() {
-    filename="${1}"
-    cat >>"${filename}" <<EOF
-
-. /usr/share/bash-completion/bash_completion
-EOF
-}
-
-ensure_bash_completion /root/.bashrc
-ensure_bash_completion /home/vscode/.bashrc
+    py3-wheel-doc \
+    black \
+    poetry
 
 # Python
 # -----------------------------------------------------------------------------
 
 pip3 install --no-cache-dir --upgrade pip
+
+pip3 install --no-cache pipx
+
+USE_EMOJI=false
+export USE_EMOJI
+
+PIPX_HOME=/usr/local/pix
+export PIPX_HOME
+
+PIPX_BIN_DIR=/usr/local/bin
+export PIPX_BIN_DIR
+
+PATH="${PIPX_BIN_DIR}:${PATH}"
+export PATH
+
+pipx install reorder-python-imports
+
+# Dotfiles
+# -----------------------------------------------------------------------------
+
+configure_bashrc() {
+    filename="${1}"
+    cat >>"${filename}" <<EOF
+
+. /usr/share/bash-completion/bash_completion
+
+eval "\$(register-python-argcomplete pipx)"
+EOF
+}
+
+configure_bashrc /root/.bashrc
+configure_bashrc /home/vscode/.bashrc
 
 # Cleanup
 # -----------------------------------------------------------------------------
